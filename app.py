807c8d601b681f7de2581c0385cece1136440c0e
Moustafa-Eng <mk2639087@gmail.com>
2024-06-17 14:27:07 +0300

    new

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 
 # Load the saved model
-model_path = "trained_model5_inceptionv3old.joblib"
+model_path = "trained_model5_inceptionv3old.pkl"
 try:
     loaded_model = joblib.load(model_path)
     print(f"Model loaded successfully from {model_path}")
